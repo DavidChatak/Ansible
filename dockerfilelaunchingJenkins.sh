4271edf341e7ddcc6fdec016f19aed8e888f85c5
david Chatak <davidchatak40@gmail>
2021-02-12 18:12:06 +0000

    ***

---
+++ dockerfilelaunchingJenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+useradd jenkins -m
+docker run \
+  -u jenkins \
+  --rm \
+  -d \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /home/jenkins:/var/jenkins_home \
+-t slave \
+  jenkins/jenkins:lts
+cat /home/jenkins/secrets/initialAdminPassword
+
+
